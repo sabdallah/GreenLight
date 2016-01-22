7073c8a1f023501ca1495cce0f5fce28b2e6cd59
1620032 <1620032@ROOM-287-002.lfschools.net>
2016-01-22 11:10:13 -0600

    Made the leave class servlet

---
+++ src/java/org/mypackage/controllers/LeaveClassServlet.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.mypackage.models.Room;
+import org.mypackage.models.UserDatabaseHandler;
+import org.mypackage.models.panelCreator;
+
+/**
+ *
+ * @author 1620032
+ */
+public class LeaveClassServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession ses = request.getSession(false);
+        if(ses == null){
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.html");
+            dispatcher.forward(request, response);
+        }
+        
+        ses.setAttribute("ClassNum", null);
+        
+        if (ses.getAttribute("understand") != null) {
+            Room room = new Room(Integer.parseInt((String) ses.getAttribute("roomNum")));
+            room.removeKid();
+            if ((boolean) ses.getAttribute("understand") == true) {
+                room.removeGood();
+            }
+            ses.setAttribute("understand",null); 
+            room.loadData();
+        }
+        
+        String username = (String)ses.getAttribute("username");
+        PrintWriter out = response.getWriter();
+        new panelCreator(out, username, UserDatabaseHandler.isTeacher(username));
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
